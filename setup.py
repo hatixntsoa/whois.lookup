4ba2c13ceae959d55a9fb516e5a503640a2bd24f
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-09-25 22:16:50 +0300

    New Version for PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylookup-tool',
-    version='0.1',
+    version='0.1.1',
     description='A CLI tool for WHOIS Lookup Operations',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
